a3acdd3a744a5cb7cc3c9c3d6f1e75594a5ee42b
Julian Nadeau <julian.nadeau@jadedpixel.com>
2014-06-25 15:37:10 -0400

    Fix CX Bash

--- .cloud66.bash
+++ .cloud66.bash
@@ -1,2 +1,2 @@
-alias cx_shopify='cp ~/.cloud66/cx_shopify.json ~/.cloud66/cx.json'
-alias cx_cfk='cp ~/.cloud66/cx_cfk.json ~/.cloud66/cx.json'
+alias cx_shopify='cp ~/.cloud66_backup/cx_shopify.json ~/.cloud66/cx.json'
+alias cx_cfk='cp ~/.cloud66_backup/cx_cfk.json ~/.cloud66/cx.json'
