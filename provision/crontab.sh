fa5bcd874e869d899efa65025c61c51a65528b28
Julian Nadeau <julian@jnadeau.ca>
2016-01-19 22:34:33 -0500

    Fix crontab script

--- provision/crontab.sh
+++ provision/crontab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-crontab -l > mycron # Copy current Crontab
+mycron=""
 
 echo "*/5 * * * * \"/Users/juliannadeau/Google Drive/WriteApp/Mobile Infrastructure/commit.sh\" >/tmp/stdout.log 2>/tmp/stdout.log" >> mycron
 echo "0 22 * * * ruby /Users/juliannadeau/Development/other/dotfiles/support/git_backup.rb \"/Users/juliannadeau/Dropbox (Personal)/Backup/repo_list.json\"" >> mycron
