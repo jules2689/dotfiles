851e19545fe50ed3264d8792a5bb1bb30c1e34ef
Julian Nadeau <julian@jnadeau.ca>
2016-01-29 19:49:16 -0500

    Add some more various options

--- provision/various.sh
+++ provision/various.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -eo pipefail
 
+echo "Setting global github settings"
+git config --global user.name "Julian Nadeau"
+git config --global user.email "julian@jnadeau.ca"
+
+echo "Setting some default system settings"
 DevToolsSecurity -enable
