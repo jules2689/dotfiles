6fe70fc9ca2e8f786cee46c42ddc5be18263bc7a
Julian Nadeau <julian@jnadeau.ca>
2016-09-21 23:59:06 -0400

    Update crontab.sh

--- provision/crontab.sh
+++ provision/crontab.sh
@@ -3,7 +3,6 @@
 mycron=""
 
 echo "*/5 * * * *  /bin/bash \"/Users/juliannadeau/Google Drive/WriteApp/Mobile Infrastructure/commit.sh\" >/tmp/stdout.log 2>/tmp/stdout.log" >> mycron
-echo "0 22 * * * ruby /Users/juliannadeau/Development/personal/other/dotfiles/support/git_backup.rb \"/Users/juliannadeau/Dropbox/Backup/repo_list.json\"" >> mycron
 
 crontab mycron # Install crontab
 rm mycron
