21f58e953af5a6e376b6f1241c18e88895a7860e
Julian Nadeau <julian@jnadeau.ca>
2017-03-02 14:45:07 -0500

    Update mas.sh

--- provision/mas.sh
+++ provision/mas.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
 if [[ ! -d /Applications/Xcode.app ]]; then
+  echo "Installing Xcode"
   mas install 497799835
 fi
 
 if [[ ! -d /Applications/Write.app ]]; then
+  echo "Installing Write app"
   mas install 848311469
 fi
 
 if [[ ! -d /Applications/Pocket.app ]]; then
+  echo "Installing Pocket"
   mas install 568494494
 fi
 
 if [[ ! -d /Applications/GIF\ Brewery\ 3.app ]]; then
+  echo "Installing Gif Brewery 3"
   mas install 1081413713
 fi
 
 if [[ ! -d /Applications/Irvue.app ]]; then
+  echo "Installing Irvue"
   mas install 1039633667
 fi
+
+ echo "Finished Installing stuff"
 
 outdated=$(mas outdated)
 if [[ -z "${outdated// }" ]]; then
